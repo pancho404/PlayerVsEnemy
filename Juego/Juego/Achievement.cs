c7b9c7487aade592498a488cc36054fa1b0f47a6
pancho404 <franciscosanchez2101@gmail.com>
2022-05-10 12:33:04 -0300

    Added achievement class.

---
+++ Juego/Juego/Achievement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego
+{
+    abstract class Achievement
+    {
+
+
+        public abstract void SendMessage();
+    }
+}

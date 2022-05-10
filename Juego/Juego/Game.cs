e0100a858a6ff756b12ca46245a586538b48e530
pancho404 <franciscosanchez2101@gmail.com>
2022-05-10 12:33:29 -0300

    Enemy tipes implemented.

--- Juego/Juego/Game.cs
+++ Juego/Juego/Game.cs
@@ -13,9 +13,13 @@ namespace Juego
         Player playerOne;
         Player playerTwo;
         Enemy enemy;
-        UI uI;
+        DiagonalEnemy diagonalEnemy;
+        LinearEnemy linearEnemy;
+        UI playerOneUI;
+        UI playerTwoUI;
         PowerUp powerUp;
         Random rnd;
+       
         public void Run()
         {
             Init();
@@ -34,8 +38,11 @@ namespace Juego
             powerUp = new PowerUp("O", rnd.Next(5, 8), rnd.Next(5, 8), true, rnd);
             playerOne = new Player("P", 0, 0, 5, 0, ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.RightArrow, ConsoleKey.LeftArrow);
             playerTwo = new Player("T", 4, 4, 5, 0, ConsoleKey.W, ConsoleKey.S, ConsoleKey.D, ConsoleKey.A);
-            uI = new UI(playerOne, playerTwo);
+            playerOneUI = new UI(playerOne, 100, 0);
+            playerTwoUI = new UI(playerTwo, 100, playerOneUI.PosY+playerOneUI.Offset);
             enemy = new Enemy("E", 10, 0, rnd);
+            diagonalEnemy = new DiagonalEnemy("D", 10, 5, rnd);
+            linearEnemy = new LinearEnemy("L", 10, 8, rnd);
         }
 
         public void Update()
@@ -60,32 +67,49 @@ namespace Juego
                     powerUp.Active = true;
                 }
             }
-            if (playerTwo.CheckCollision(enemy))
+            CheckCollisions();
+            enemy.Move();
+            diagonalEnemy.DiagonalMove();
+            linearEnemy.LinearMove();
+        }
+
+        private void CheckCollisions()
+        {
+            CheckEnemyCollision(enemy, playerOne);
+            CheckEnemyCollision(enemy, playerTwo);
+            CheckEnemyCollision(diagonalEnemy, playerOne);
+            CheckEnemyCollision(diagonalEnemy, playerTwo);
+            CheckEnemyCollision(linearEnemy, playerOne);
+            CheckEnemyCollision(linearEnemy, playerTwo);
+            CheckPowerUpCollision(playerOne);
+            CheckPowerUpCollision(playerTwo);
+        }
+
+        private void CheckPowerUpCollision(Player player)
+        {
+            if (player.CheckCollision(powerUp))
             {
-                if (!playerTwo.AttackMode)
+                powerUp.Move();
+                player.EnterAttackMode();
+                powerUp.Active = false;
+            }
+        }
+
+        private void CheckEnemyCollision(Enemy enemy, Player player)
+        {
+            if (player.CheckCollision(enemy))
+            {
+                if (!player.AttackMode)
                 {
-                    DecreasePlayerLives(playerTwo);
+                    DecreasePlayerLives(player);
                 }
-                else if (playerTwo.AttackMode)
+                else if (player.AttackMode)
                 {
-                    IncreasePlayerPoints(playerTwo);
-                    playerTwo.AttackMode = false;
+                    IncreasePlayerPoints(player);
+                    player.AttackMode = false;
                     powerUp.Active = true;
                 }
             }
-            if (playerOne.CheckCollision(powerUp))
-            {
-                powerUp.Move();
-                playerOne.EnterAttackMode();
-                powerUp.Active = false;
-            }
-            if (playerTwo.CheckCollision(powerUp))
-            {
-                powerUp.Move();
-                playerTwo.EnterAttackMode();
-                powerUp.Active = false;
-            }
-            enemy.Move();
         }
 
         public void Draw()
@@ -93,11 +117,14 @@ namespace Juego
             playerOne.DrawCharacter();
             playerTwo.DrawCharacter();
             enemy.DrawCharacter();
+            diagonalEnemy.DrawCharacter();
+            linearEnemy.DrawCharacter();
             if (powerUp.Active)
             {
                 powerUp.DrawCharacter();
             }
-            uI.DrawUI();
+            playerOneUI.DrawUI();
+            playerTwoUI.DrawUI();
             Thread.Sleep(200);
             Console.Clear();
         }
